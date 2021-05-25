11369bc8bbd1f018d86be7ab95d82a0f78fd0b3e
Yathish Poojary <yathishpoojary023@gmail.com>
2021-05-25 15:07:47 +0530

    updated app.js

--- service/app.js
+++ service/app.js
@@ -18,21 +18,4 @@ app.use(cors());
 
 app.use("/api/user", userRouter);
 
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-console.log("helo");
 module.exports = app;
