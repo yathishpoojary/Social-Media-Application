c3fc41fb5d0dac3e6b9afafcd6c491212ec7ca24
Yathish Poojary <yathishpoojary023@gmail.com>
2021-05-25 14:53:12 +0530

    Removed console and updated router

--- service/database/userInfoDB.js
+++ service/database/userInfoDB.js
@@ -1,10 +1,56 @@
+const { connection } = require("mongoose");
 const db = require("./db");
 
 function getUserInfo(userInfo, done) {
-    db.connectToServer();
+    const connection = db.connection();
+    connection.findOne(userInfo)
+        db.getDb('UserCluster').find().toArray()
+        .then(results => {
+            done(null, results);
+        })
+        .catch(error => {
+            done(error, null);
+        })
+}
 
-    db.getDb('UserCluster').find().toArray()
-      .then(results => {
+function getAllUserInfo(done) {
+    const connection = db.connection();
+    connection.find().toArray()
+    .then(results => {
+        done(null, results);
+      })
+      .catch(error => {
+          done(error, null);
+      })
+}
+
+function addUserInfo(userInfo, done) {
+    const connection = db.connection();
+    connection.insertOne(userInfo);
+    patientCollection.insertOne(userInfo)
+    .then(results => {
+        done(null, results);
+      })
+      .catch(error => {
+          done(error, null);
+      })
+}
+
+function updateUserInfo(userInfo, done) {
+    const connection = db.connection();
+    connection.findOneAndUpdate(userInfo)
+    .then(results => {
+        done(null, results)
+      })
+      .catch(error => {
+          done(error, null)
+      })
+}
+
+function deleteUserInfo(userInfo, done) {
+    const connection = db.connection();
+    connection.deleteOne(userInfo)
+    .then(results => {
         done(null, results)
       })
       .catch(error => {
@@ -13,5 +59,9 @@ function getUserInfo(userInfo, done) {
 }
 
 module.exports = {
-    getUserInfo
+    getUserInfo,
+    addUserInfo,
+    updateUserInfo,
+    deleteUserInfo,
+    getAllUserInfo
 }
--- service/routes/userRouter.js
+++ service/routes/userRouter.js
@@ -8,6 +8,21 @@ router.get("getUser/:userInfo", function(req, resp, next) {
     if(!userInfo) {
         resp.status(400).send({error: "invalid Data"});
     }
+    userInfoDB.getUserInfo(userInfo, function(err, result) {
+            if(err) {
+                resp.status(400).send(err);
+            } else {
+                resp.status(200).send(result);
+            }
+        });
+
+});
+
+router.get("getAllUser", function(req, resp, next) {
+    let userInfo = req.params.userInfo;
+    if(!userInfo) {
+        resp.status(400).send({error: "invalid Data"});
+    }
     userInfoDB.getAllTestList(userInfo, function(err, result) {
             if(err) {
                 resp.status(400).send(err);
@@ -16,6 +31,48 @@ router.get("getUser/:userInfo", function(req, resp, next) {
             }
         });
 
-})
+});
+
+router.post("addUser", function(req, resp, next) {
+    let userInfo = req.body;
+    if(!userInfo) {
+        resp.status(400).send({error: "invalid Data"});
+    }
+    userInfoDB.addUserInfo(userInfo, function(err, result) {
+    if(err) {
+        resp.status(400).send(err);
+    } else {
+        resp.status(200).send(result);
+        }
+    });
+});
+
+router.post("updateUser", function(req, resp, next) {
+    let userInfo = req.body;
+    if(!userInfo) {
+        resp.status(400).send({error: "invalid Data"});
+    }
+    userInfoDB.updateUserInfo(userInfo, function(err, result) {
+        if(err) {
+            resp.status(400).send(err);
+        } else {
+            resp.status(200).send(result);
+        }
+    });
+});
+
+router.post("deleteUser", function(req, resp, next) {
+    let userInfo = req.body;
+    if(!userInfo) {
+        resp.status(400).send({error: "invalid Data"});
+    }
+    userInfoDB.deleteUserInfo(userInfo, function(err, result) {
+        if(err) {
+            resp.status(400).send(err);
+        } else {
+            resp.status(200).send(result);
+        }
+    });
+});
 
 module.exports = router;
